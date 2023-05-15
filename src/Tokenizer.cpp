5101fccc3dfc5e1af1a783b35ba4b9d47ec2f56b
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2023-05-15 18:16:13 -0400

    Tokenizer

---
+++ src/Tokenizer.cpp
@@ -0,0 +1,237 @@
+#include "Tokenizer.hpp"
+#include <sstream>
+#include <vector>
+#include <map>
+
+Token Token::EndOfFileToken = { TokenType::Eof, "", 0, 0 };
+
+Tokenizer::Tokenizer(std::istream& inStream) : in(inStream), currentToken(0) { }
+
+std::map<std::string, TokenType> tokenMap = {
+        {"import", TokenType::Import},
+        {"var", TokenType::Var},
+        {"const", TokenType::Const},
+        {"struct", TokenType::Struct},
+        {"enum", TokenType::Enum},
+        {"while", TokenType::While},
+        {"match", TokenType::Match},
+        {"if", TokenType::If},
+        {"else", TokenType::Else},
+        {"return", TokenType::Return},
+        {"pub", TokenType::Pub},
+        {"fn", TokenType::Fn},
+        {"u8", TokenType::U8},
+        {"u16", TokenType::U16},
+        {"u32", TokenType::U32},
+        {"u64", TokenType::U64},
+        {"i8", TokenType::I8},
+        {"i16", TokenType::I16},
+        {"i32", TokenType::I32},
+        {"i64", TokenType::I64},
+        {"f32", TokenType::F32},
+        {"f64", TokenType::F64},
+        {"true", TokenType::True},
+        {"false", TokenType::False},
+};
+
+auto Tokenizer::differentiate(const std::string& in) -> TokenType {
+    if(tokenMap.find(in) != tokenMap.end())
+        return tokenMap[in];
+    else
+        return TokenType::Identifier;
+}
+
+auto Tokenizer::tokenize_line(std::string input, int lineNumber) -> void {
+    auto len = input.length();
+
+    std::string current_token;
+
+    auto addRemainingToken = [&](uint32_t i) {
+        if (!current_token.empty()) {
+            auto t = differentiate(current_token);
+            if(t == TokenType::Identifier && current_token[0] >= '0' && current_token[0] <= '9') {
+                t = TokenType::Numeric;
+            }
+
+            tokens.push_back({t, current_token, static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+            current_token.clear();
+        }
+    };
+
+    for(auto i = 0; i < len; i++) {
+        auto c = input[i];
+
+        switch (c) {
+
+            case ':': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Colon, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '=': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Equal, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '<': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::LessThan, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '>': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::GreaterThan, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '|': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Or, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '&': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::And, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '!': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Not, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '+': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Plus, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '-': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Minus, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '*': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Multiply, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '/': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Divide, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '%': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Modulo, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '^': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Exponent, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '\"': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Quote, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case ',': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Comma, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '\'': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::Apostrophe, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '(': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::LeftParen, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case ')': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::RightParen, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '{': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::LeftBrace, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '}': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::RightBrace, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case '[': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::LeftBracket, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case ']': {
+                addRemainingToken(i);
+                tokens.push_back({TokenType::RightBracket, std::string(1, c), static_cast<uint32_t>(lineNumber), static_cast<uint32_t>(i)});
+                break;
+            }
+
+            case ' ':
+            case '\t':
+            case '\n':
+            case '\r':
+            case '\f':
+            case '\v': {
+                addRemainingToken(i);
+                break;
+            }
+
+            default: {
+                if(c != ';')
+                    current_token += c;
+                break;
+            }
+        }
+    }
+    addRemainingToken(len);
+}
+
+auto Tokenizer::tokenize_all() -> void {
+    std::string input;
+
+    int currLine = 1;
+    while(std::getline(in, input)) {
+        tokenize_line(input, currLine++);
+    }
+}
+
+auto Tokenizer::next() -> Token {
+    if(tokens.size() > currentToken) {
+        return tokens[currentToken++];
+    }
+
+    return Token::EndOfFileToken;
+}
+
+auto Tokenizer::get_tokens() -> const std::vector<Token>& {
+    return tokens;
+}
---
+++ src/Tokenizer.hpp
@@ -0,0 +1,241 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+enum class TokenType {
+    Unknown = -1,
+    Eof,
+    Import,
+    Var,
+    Const,
+    Struct,
+    Enum,
+    While,
+    Match,
+    If,
+    Else,
+    Return,
+    Pub,
+    Fn,
+    U8, U16, U32, U64,
+    I8, I16, I32, I64,
+    F32, F64,
+    True,
+    False,
+    Numeric,
+    LeftParen,
+    LeftBracket,
+    LeftBrace,
+    RightParen,
+    RightBracket,
+    RightBrace,
+    Colon,
+    Equal,
+    LessThan,
+    GreaterThan,
+    Or,
+    And,
+    Not,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Modulo,
+    Exponent,
+    Quote,
+    Apostrophe,
+    Comma,
+    Identifier
+};
+
+inline auto token_to_string(TokenType type) -> std::string {
+    switch(type) {
+        case TokenType::Unknown:
+            return "unknown";
+            
+        case TokenType::Eof:
+            return "EOF";
+            
+        case TokenType::Import:
+            return "Import";
+            
+        case TokenType::Var:
+            return "Var";
+            
+        case TokenType::Const:
+            return "Const";
+            
+        case TokenType::Struct:
+            return "Struct";
+            
+        case TokenType::Enum:
+            return "Enum";
+            
+        case TokenType::While:
+            return "While";
+            
+        case TokenType::Match:
+            return "Match";
+            
+        case TokenType::If:
+            return "If";
+            
+        case TokenType::Else:
+            return "Else";
+            
+        case TokenType::Return:
+            return "Return";
+
+        case TokenType::Numeric:
+            return "Numeric";
+
+        case TokenType::Pub:
+            return "Pub";
+            
+        case TokenType::Fn:
+            return "Fn";
+            
+        case TokenType::U8:
+            return "U8";
+            
+        case TokenType::U16:
+            return "U16";
+            
+        case TokenType::U32:
+            return "U32";
+            
+        case TokenType::U64:
+            return "U64";
+            
+        case TokenType::I8:
+            return "I8";
+            
+        case TokenType::I16:
+            return "I16";
+            
+        case TokenType::I32:
+            return "I32";
+            
+        case TokenType::I64:
+            return "I64";
+            
+        case TokenType::F32:
+            return "F32";
+            
+        case TokenType::F64:
+            return "F64";
+            
+        case TokenType::True:
+            return "True";
+            
+        case TokenType::False:
+            return "False";
+            
+        case TokenType::LeftParen:
+            return "LeftParen";
+            
+        case TokenType::LeftBracket:
+            return "LeftBracket";
+            
+        case TokenType::LeftBrace:
+            return "LeftBrace";
+            
+        case TokenType::RightParen:
+            return "RightParen";
+            
+        case TokenType::RightBracket:
+            return "RightBracket";
+            
+        case TokenType::RightBrace:
+            return "RightBrace";
+            
+        case TokenType::Colon:
+            return "Colon";
+            
+        case TokenType::Equal:
+            return "Equal";
+            
+        case TokenType::LessThan:
+            return "LessThan";
+            
+        case TokenType::GreaterThan:
+            return "GreaterThan";
+            
+        case TokenType::Or:
+            return "Or";
+            
+        case TokenType::And:
+            return "And";
+            
+        case TokenType::Not:
+            return "Not";
+            
+        case TokenType::Plus:
+            return "Plus";
+            
+        case TokenType::Minus:
+            return "Minus";
+            
+        case TokenType::Multiply:
+            return "Multiply";
+            
+        case TokenType::Divide:
+            return "Divide";
+            
+        case TokenType::Modulo:
+            return "Modulo";
+            
+        case TokenType::Exponent:
+            return "Exponent";
+            
+        case TokenType::Quote:
+            return "Quote";
+            
+        case TokenType::Apostrophe:
+            return "Apostrophe";
+            
+        case TokenType::Comma:
+            return "Comma";
+            
+        case TokenType::Identifier:
+            return "Identifier";
+            
+    }
+
+    return "unknown";
+}
+
+struct Token {
+    Token() = default;
+
+    TokenType type = TokenType::Unknown;
+    std::string value;
+
+    // Metadata
+    uint32_t line = 0;
+    uint32_t column = 0;
+
+    [[nodiscard]] inline auto to_string() const -> std::string {
+        return R"({"tokenType": ")" + token_to_string(type) + R"(", "value": ")" + value + R"(", "line": )" + std::to_string(line) + R"(, "column": )" + std::to_string(column) + "}";
+    }
+
+    static Token EndOfFileToken;
+};
+
+class Tokenizer {
+public:
+    explicit Tokenizer(std::istream& inStream);
+
+    auto tokenize_all() -> void;
+    auto next() -> Token;
+    auto get_tokens() -> const std::vector<Token>&;
+private:
+    auto tokenize_line(std::string input, int lineNumber) -> void;
+    static auto differentiate(const std::string& in) -> TokenType;
+
+    uint32_t currentToken;
+    std::vector<Token> tokens;
+    std::istream& in;
+};
